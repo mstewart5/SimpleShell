f4d69a5b61937638b415440347f1de54dc812d25
mstewart5 <104705110+mstewart5@users.noreply.github.com>
2023-04-27 15:47:07 -0500

    Comment functions
    Added documentation briefly explaining the purpose of each function.

--- src/functions.h
+++ src/functions.h
@@ -1,3 +1,4 @@
+//printTitle() was given by the textbook, it prints "A Simple Shell" in an ASCII stylized format
 void printTitle() {
     printf("              ___       _____ _                 __        _____ __         ____\n");
     printf("             /   |     / ___/(_)___ ___  ____  / /__     / ___// /_  ___  / / /\n");
@@ -6,29 +7,35 @@ void printTitle() {
     printf("          /_/  |_|   /____/_/_/ /_/ /_/ .___/_/\\___/   /____/_/ /_/\\___/_/_/\n");
 }
 
-//Build the prompt string to have the machine name, current directory, or other desired info
+/*
+printPrompt() gives the user a prompt for input. The function declares two variables to store machine name and directory.
+Using built in C functions, the function stores the relevant information in the variables and then prints the prompt
+*/
 void printPrompt() {
-    char machineName[1024];
-    char currentDirectory[1024];
+    char machineName[1024]; // char array to store the name of the machine
+    char currentDirectory[1024]; // char array to store the current directory
 
     gethostname(machineName, sizeof(machineName));
     getcwd(currentDirectory, sizeof(currentDirectory));
     printf("%s:%s>", machineName, currentDirectory);
 }
 
-//This code uses any set of I/O functions, such as those in the stdio library
-//to read the entire command line into the buffer.
-//This implementation is greatly simplified, but it does the job.
+/*
+readCommand() uses the built in C function fgets() to read input from the user given by the "buffer" parameter. The arguments passed through fgets()
+ensure the user does not go over a specified length.
+*/
 void readCommand(char *buffer) {
-    fgets(buffer, MAX_ARG_LEN, stdin); // used fgets to avoid warnings every compile
+    fgets(buffer, MAX_ARG_LEN, stdin);
 }
 
-//Determine command name and construct the parameter list. This function will build argv[] and set the argc value.
-//argc is the number of "tokens" or words on the command line. argv[] is an array of strings (pointers to char *).
-//The last element in argv[] much be NULL. As we scan the command line from the left, the first token goes in argv[0], 
-//the second in arg[1], and so on. Each time we add a token to argv[], we increment argc.
+/*
+parseCommand() determines command name and constructs the parameter list. This function will build argv[] and set the argc value.
+argc is the number of "tokens" or words on the command line. argv[] is an array of strings (pointers to char *).
+The last element in argv[] much be NULL. As we scan the command line from the left, the first token goes in argv[0], 
+the second in arg[1], and so on. Each time we add a token to argv[], we increment argc.
+*/
 int parseCommand(char *cLine, struct command_t *cmd) {
-    int argc;
+    int argc; // variable to store num of tokens from user input
     char **clPtr;
 
     //Initialization
@@ -48,13 +55,17 @@ int parseCommand(char *cLine, struct command_t *cmd) {
     return 1;
 }
 
-//this function reads the path variable for this environment, then builds an array, dirs[], of the directories in path
+/*
+parsePath() reads from path variable for this envirornment and builds an array, dirs[], of the directories in path.
+This functions ensures that lookupPath() can verify whether a command existes. Returns the number of properly parsed directories.
+*/
 int parsePath(char* dirs[]) {  
-    char *pathEnvVar;
-    char *thePath;
+    char *pathEnvVar; // variable to store paths
+    char *thePath; // variable to store information from pathEnvVar so that it can be parsed
 
-    for (int i = 0; i < MAX_ARGS; i++)
+    for (int i = 0; i < MAX_ARGS; i++) // initialize all directories to be NULL
         dirs[i] = NULL;
+
     pathEnvVar = (char *) getenv("PATH");
     thePath = (char *) malloc(strlen(pathEnvVar) + 1);
     strcpy(thePath, pathEnvVar);
@@ -63,25 +74,27 @@ int parsePath(char* dirs[]) {
     char *parse = strtok(thePath, ":");
     int parsedString = 0;
 
-    // parsing every directory using strtok
+    // Parsing every directory using strtok
     while (parse != NULL) {
         dirs[parsedString] = malloc(strlen(parse) + 1); // store the parsed paths in dirs[]
         strcpy(dirs[parsedString], parse);
-        parsedString++;
-        parse = strtok(NULL, ":");
+        parsedString++; // increment number of parsed directories
+        parse = strtok(NULL, ":"); // parse next directory
     }
 
     return parsedString; // return number of parsed directories
 
 }
 
-//This function searches the directiories identified by the dir arguemnt to see if argv[0] (the file name) appears there.
-//Allocate a new string, place the full path name in it, then return the string.
+/*
+lookupPath() searches the directiories identified by the dir arguemnt to see if argv[0] (the file name) appears there.
+Allocate a new string, place the full path name in it, then return the string.
+*/
 char *lookupPath(char **argv, char **dir) {
-    char *result;
-    char pName[MAX_PATH_LEN];
+    char *result; // variable to store the path and return it
+    char pName[MAX_PATH_LEN]; // temp var to update result
 
-    if (strcmp(argv[0], "exit") == 0)
+    if (strcmp(argv[0], "exit") == 0) // if user input is exit, terminate the program
         exit(0);
 
     //check to see if file name is already an absolute path name
@@ -125,10 +138,12 @@ char *strsep(char **stringp, const char *delim) {
     return rv;
 }
 
-// This function exists for the multithreaded version of the shell.
-// Because pthreads can only take one argument for a function it is necessary to 
-// wrap execv() so that it can work given only one argument.
-// The argument in question being the commant_t struct.
+/*
+execWrapper() exclusively for the multithreaded version of the shell.
+Because pthreads can only take one argument for a function it is necessary to 
+wrap execv() so that it can work given only one argument.
+The argument in question being the commant_t struct.
+*/
 void *execWrapper(void *arg) {
     struct command_t *cmd = (void *) arg;
     execv(cmd->name, cmd->argv);
