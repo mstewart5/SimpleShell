d29dc43735b00d70b858f5c5753405ee1f222089
mstewart5 <104705110+mstewart5@users.noreply.github.com>
2023-04-27 15:11:28 -0500

    Add documentation to shell.c and multithreadshell.c
    Removed "int i" as it did nothing in the program.

--- src/multithreadshell.c
+++ src/multithreadshell.c
@@ -1,31 +1,50 @@
+/*
+Authors: Michael Stewart, Andrew Smith, Drake Richard
+Course: COSC 4302, Operating Systems
+Assignment: Linux Project Shell
+Instructor: Bo Sun 
+
+This program was written to create a shell that operates similarly to the linux terminal. This was achieved through writing functions to read user input,
+parse the input, and then check linux directories to see if that command exists or not. If the command exists, a new thread is createde and a 
+function designed to wrap execv() is called within that thread to execute the command. 
+This shell can run commands such as "ls, man, ps, mkdir, etc...". As per the project requirements, this code was written in C.
+
+It should be noted that this program will only ever be able to run one shell command.
+execv() inherently terminates the process it was called in after a successfull call. 
+No matter how many threads there are, because the threads are contained within one process,
+execv() will still terminate the process which explains why only one shell command can ever be ran. 
+
+It should be noted that the program may take a few runs to get a successful command execution.
+If execv() fails, ctrl+c out of the program and then run it again. Repeat this until the command works. This issue may be a result of some mismanaged memory.
+*/
+
+// These headers hold code that is vital to the program functioning properly
+// Definitions.h includes several libraries, definitions, and function prototypes to avoid writing
+// redundant code.
+// Functions.h holds all the functions for formatting the terminal, building an array of directories that hold linux commands,
+// reading commands, and parsing commands from user input.
 #include <unistd.h>
 #include "definitions.h"
 #include "functions.h"
 #include <pthread.h>
 
 #define _CRT_SECURE_NO_WARNINGS
-/*
-It should be noted that this program will only ever be able to run one shell command.
-execv() inherently terminates the process it was called in after a successfull call. 
-No matter how many threads there are, because the threads are contained within one process,
-execv() will still terminate the process which explains why only one shell command can ever be ran. 
 
-I will also note that the program may take a few runs to actually get a successful command execution.
-If execv() fails, ctrl+c out of the program and then run it again. Repeat this until the command works.
-I am not sure why it does this, but I am inclined to believe there is some mismanaged memory causing the issue.
-- Michael S.
+/*
+int main() is responsible for executing the program.
+The method initializes important variables, gives the user an input prompt, and then calls several functions
+to read, parse, and execute the command given by the user.
 */
 int main() {
     // Shell initialization
-    int i;
-    int pid;
-    char commandLine[MAX_LINE_LEN];
-    char *pathv[MAX_PATHS];
-    struct command_t command;
-    pthread_t cmdExecThread;
+    int pid; // variable to check if fork() was successful or not
+    char commandLine[MAX_LINE_LEN]; // stores the user's input
+    char *pathv[MAX_PATHS]; // stores the directories holding executable commands
+    struct command_t command; // struct that contains multiple variables vital to parsing the command
+    pthread_t cmdExecThread; // initialize a thread data type for executing the command
+
     parsePath(pathv); // get directory paths from PATH
 
-    system("clear");
     while(1) {
         printPrompt();
     
@@ -34,16 +53,16 @@ int main() {
         readCommand(commandLine);
         parseCommand(commandLine, &command);
 
-        // create thread to execute command
-        // we wrap execv() in the function execWrapper due to pthreads only being able to take
+        // Create thread to execute command
+        // Wrap execv() in the function execWrapper due to pthreads only being able to take
         // one argument for a function
         pthread_create(&cmdExecThread, NULL, execWrapper, &command);
 
-        command.argv[command.argc] = NULL; // dont know what this does, but the code doesn't work without it
-        // get full pathnames
+        // Get full pathnames
+        command.argv[command.argc] = NULL;
         command.name = lookupPath(command.argv, pathv);
      
-        // await thread termination
+        // Await thread termination
         pthread_join(cmdExecThread, NULL);
         
     }
--- src/shell.c
+++ src/shell.c
@@ -1,3 +1,21 @@
+/*
+Authors: Michael Stewart, Andrew Smith, Drake Richard
+Course: COSC 4302, Operating Systems
+Assignment: Linux Project Shell
+Instructor: Bo Sun 
+
+This program was written to create a shell that operates similarly to the linux terminal. This was achieved through writing functions to read user input,
+parse the input, and then check linux directories to see if that command exists or not. If the command exists, a child process is made and execv() is called within that 
+process to execute the command. This shell can run commands such as "ls, man, ps, mkdir, etc...". As per the project requirements, this code was written in C.
+
+As of 4/27/23, there are no outstanding bugs that should be noted.
+ */
+
+// These headers hold code that is vital to the program functioning properly
+// Definitions.h includes several libraries, definitions, and function prototypes to avoid writing
+// redundant code.
+// Functions.h holds all the functions for formatting the terminal, building an array of directories that hold linux commands,
+// reading commands, and parsing commands from user input.
 #include <unistd.h>
 #include "definitions.h"
 #include "functions.h"
@@ -5,22 +23,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 
-//NOTE: From the book, this is the intital codebehaves like a shell program.You
-//can use this code as part of your solution to the UNIX shell Laboratory Exercise at the end
-//of the chapter.This program reads a list of commands from a file, then executes each command just as a shell would execute it.Each command line in the file will be the same as
-//a line you would type to the shell
-//Just implementing this for ground work, and then will go back and add what's needed...
-
+/*
+int main() is responsible for executing the program.
+The method initializes important variables, gives the user an input prompt, and then calls several functions
+to read, parse, and execute the command given by the user.
+*/
 int main(int argc, char *argv[])
 {
-    // ...
-    //Shell initialization
-    // Initializing all the variables needed for the shell to actually work
-    int i;
-    int pid;
-    char commandLine[MAX_LINE_LEN];
-    char *pathv[MAX_PATHS];
-    struct command_t command;
+    // Initializing all the variables needed for the shell to function properly
+    int pid; // variable to check if fork() was successful or not
+    char commandLine[MAX_LINE_LEN]; // stores the user's input
+    char *pathv[MAX_PATHS]; // stores the directories holding executable commands
+    struct command_t command; // struct that contains variables vital to parsing the command
+
     printTitle();
 
     parsePath(pathv); // Get directory paths from PATH
@@ -44,51 +59,5 @@ int main(int argc, char *argv[])
         wait(NULL);
     
     }
-        // figure out how to get the shell to terminate eventually - Michael S.
-
-/* // I'm commenting this out because this runs commands from a file, not from the linux directories. Should probably just outright get rid of this code - Michael S.
-    int i;
-    int pid, numChildren;
-    int status;
-    FILE* fid;
-    char cmdLine[MAX_LINE_LEN];
-    struct command_t command;
-
-    //Read the command line parameters
-    if (argc != 2) {
-        fprintf(stderr, "Usage: launch <launch_set_filename>\n");
-        exit(0);
-    }
-
-    //Open a file that conatins a set of commands
-    fid = fopen(argv[1], "r");
-
-    //Process each command in the launch file
-    numChildren = 0;
-    while (fgets(cmdLine, MAX_LINE_LEN, fid) != NULL) {
-        parseCommand(cmdLine, &command);
-        command.argv[command.argc] = NULL;
-
-        //Create a child process to execute the command
-        if ((pid = fork()) == 0) {
-
-            //Child executing command
-            execvp(command.name, command.argv);
-        }
-
-        //Parent continuing to the next command in the file
-        numChildren++;
-    }
-
-    printf("\n\nlaunch: Launched %d commands\n", numChildren);
-
-    //Terminate after all childern have terminated
-    for (int i = 0; i < numChildren; i++) {
-        wait(&status);
-        //Should free dynamic storage in command data structure
-    }
-
-    printf("\n\nlaunch: Terminating successfully\n");
-    return 0;
-*/
+        // Shell will terminate upon reading "exit" as input from the user.
 }
